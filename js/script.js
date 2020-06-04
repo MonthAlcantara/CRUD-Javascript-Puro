b113eaee508f00cc1fd72e8a9c73e70e891d3834
Montival Junior <montival_junior@yahoo.com.br>
2020-06-03 21:03:41 -0300

    adicionada funcionalidade de update

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ window.addEventListener('load', start);
 
 var globalNames = ['Um', 'Dois', 'Tres', 'Quatro'];
 var inputName = null;
+var isEditing = false;
+var currentIndice = null;
+
+//Função Main
 
 function start(event) {
   inputName = document.querySelector('#inputName');
@@ -9,6 +13,8 @@ function start(event) {
   render();
   activateInput();
 }
+
+//Função Form
 
 function preventFormSubmit() {
   function handleFormSubmit(event) {
@@ -18,38 +24,84 @@ function preventFormSubmit() {
   form.addEventListener('submit', handleFormSubmit);
 }
 
+//Função Input
+
 function activateInput() {
+  //Função Inserir Nome
+
   function insertName(newName) {
     globalNames.push(newName);
     render();
   }
+
+  //Função Atualizar nome
+  function updateName(newName) {
+    console.log(newName);
+    console.log(currentIndice);
+    globalNames[currentIndice] = newName;
+  }
+
+  //Função Enter Acionado
+
   function handleTyping(event) {
     if (event.key === 'Enter') {
-      insertName(event.target.value);
+      if (isEditing) {
+        updateName(inputName.value);
+      } else {
+        insertName(event.target.value);
+      }
+      isEditing = false;
+      clearInput();
     }
   }
 
   inputName.addEventListener('keyup', handleTyping);
   inputName.focus();
 }
+
+//Função RENDERIZAR
+
 function render() {
   var divNames = document.querySelector('#names');
   divNames.innerHTML = '';
   var ul = document.createElement('ul');
 
+  //Função SPAN
+
+  function createSpan(currentName, indice) {
+    //Função de Edição do Item
+
+    function editItem() {
+      isEditing = true;
+      currentIndice = indice;
+      inputName.value = currentName;
+      inputName.focus();
+    }
+    var span = document.createElement('span');
+    span.classList.add('clickable');
+    span.textContent = currentName;
+    span.addEventListener('click', editItem);
+    return span;
+  }
+
+  //Loop Criação da Lista
+
   for (var i = 0; i < globalNames.length; i++) {
     var currentName = globalNames[i];
+
     var li = document.createElement('li');
     var button = createDeleteButton(i);
-    var span = document.createElement('span');
-    span.textContent = currentName;
+    var span = createSpan(currentName, i);
+
     li.appendChild(button);
     li.appendChild(span);
+
     ul.appendChild(li);
-    clearInput();
   }
   divNames.appendChild(ul);
+  clearInput();
 
+  //Função Delete
   function createDeleteButton(indice) {
     function deleteNames() {
       globalNames.splice(indice, 1);
@@ -63,6 +115,7 @@ function render() {
     return button;
   }
 }
+//Função Limpa área Input
 function clearInput() {
   inputName.value = '';
   inputName.focus();
